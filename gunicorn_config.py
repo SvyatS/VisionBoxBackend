69ef8f0a246238262ac8c9d5de746020a2859a44
SvyatS <prisenko2002@gmail.com>
2022-08-18 08:09:03 +0700

    fix: pipeline

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
-command = '/var/www/web/sites/vision-box-staging'
+command = '/root/.local/lib/python3.8/site-packages/gunicorn'
 pythonpath = '/usr/bin/python3'
 bind = '127.0.0.1:8000'
-workers = 5
+workers = 3
 user = 'root'
 limit_request_fields = 32000
 limit_request_field_size = 0

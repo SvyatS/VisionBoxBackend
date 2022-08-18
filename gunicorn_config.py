3dde34e66b923bb3c775140b2c2c06a06b79c97d
SvyatS <prisenko2002@gmail.com>
2022-08-18 07:15:13 +0700

    fix: pipeline

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/var/www/web/sites/vision-box-staging'
+pythonpath = '/usr/bin/python3'
+bind = '127.0.0.1:8001'
+workers = 5
+user = 'root'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=settings.settings'

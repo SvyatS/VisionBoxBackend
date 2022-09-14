de1404dc29e085e933335f79927e106066b5b8bb
SvyatS <prisenko2002@gmail.com>
2022-09-14 09:15:16 +0700

    fix: settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 from secrets import token_hex
@@ -26,9 +27,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=30),
-    'SIGNING_KEY': token_hex(64)
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'SLIDING_TOKEN_LIFETIME': timedelta(hours=2),
+    # 'SIGNING_KEY': token_hex(64)
 }
 
 
@@ -133,11 +134,13 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # '/var/www/static/',
-]
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     # '/var/www/static/',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
